746d45d3c9800be075699c7d96cfe03eac7ca890
Joey Braspenning <joeybraspenning@gmail.com>
2019-03-11 23:21:52 +0100

    Multilayer with backpropagation for MNIST

--- test.py
+++ test.py
@@ -28,11 +28,53 @@ def MNIST_net(x, weights_1, weights_2):
    except IndexError:
       b = sigmoid(weights_2.dot(np.append(a, 1)))
 
-   return (np.argmax(b, axis=0), b)
+   return (np.argmax(b, axis=0), b, a)
+
+
+def mse_backprop(w1, w2, image_in, image_out):
+   b_max, b, a = MNIST_net(image_in, w1, w2)
+
+   try:
+      b_temp = b.copy()
+      b_temp[:, np.where(b_max == image_out)] = 0
+      delta_2 = (1 - b_temp).dot(np.append(a.T, np.ones(int(a.size/30)).reshape(-1,1), axis=1))
+      delta_1 = (a*(1-a) * (1-b_temp).T.dot(weights_2).T[:30,:]).dot(image_in.T)
+
+
+   except IndexError:
+      # delta_2 = (b_max != image_out) * (np.sum(b)*(1 - b)).reshape(-1, 1).dot(np.append(a, 1).reshape(-1, 1).T)
+
+
+      delta_21 =  (((1 - b/np.sum(b)) * (1-b)).reshape(-1, 1)).dot(np.append(a, 1).reshape(-1, 1).T)
+      delta_21[range(10) != image_out, :] = 0
+
+      delta_22 = ((  (1 - b/np.sum(b)) * (b*(1-b))/(np.sum(b) - b) ).reshape(-1, 1)).dot(np.append(a, 1).reshape(-1, 1).T)
+      delta_22[range(10) == image_out, :] = 0
+
+
+      delta_2 = -delta_21 + delta_22
+
+      temp1 = (((1 - b/np.sum(b)) * (1-b)).reshape(1, -1))
+      temp1[:, range(10) != image_out] = 0
+      delta_11 = ( temp1.dot(weights_2[:, :30]) * (a*(1-a)).reshape(1,-1) ).T.dot(image_in.reshape(1, -1))
+
+      temp2 =((  (1 - b/np.sum(b)) * (b*(1-b))/(np.sum(b) - b) ).reshape(1, -1))
+      temp2[:, range(10) == image_out] = 0
+      delta_12 = ( temp2.dot(weights_2[:, :30]) * (a*(1-a)).reshape(1,-1) ).T.dot(image_in.reshape(1, -1))
+
+      delta_1 = -delta_11 + delta_12
+
+
+      # delta_1 = np.zeros_like(delta_1)
+
+
+   return delta_1, delta_2
+
+
 
 
 def mse(w1, w2, image_in, image_out):
-   b_max, b = MNIST_net(image_in, w1, w2)
+   b_max, b, a = MNIST_net(image_in, w1, w2)
    # accuracy = 100*np.sum(image_out == b_max)/len(image_out)^M
    pos = np.where(b_max != image_out)[0]
    # print([[j, idx] for j, idx in zip(pos, image_out[pos])])
@@ -45,17 +87,17 @@ def mse(w1, w2, image_in, image_out):
 
 
 def mse_logcrossentropy(w1, w2, image_in, image_out):
-   b_max, b = MNIST_net(image_in, w1, w2)
+   b_max, b, a= MNIST_net(image_in, w1, w2)
 
    log1 = np.sum(np.log( np.array([b[m,p] for p, m in enumerate(image_out)]) / np.sum(b, axis=0)))
 
    log2 = np.sum(np.log(1 -   b / np.sum(b, axis=0)  )) - np.sum(np.log(1 - np.array([b[m,p] for p, m in enumerate(image_out)]) / np.sum(b, axis=0)))
 
-   return -log1 + log2
+   return  -log1 + log2
 
 
 def grdmse(weights_1, weights_2, temp1, temp2, image_in, image_out):
-   eps = 0.001
+   eps = 0.01
    grad_1 = np.zeros(weights_1.shape)
    grad_2 = np.zeros(weights_2.shape)
    for i in range(weights_1.size + weights_2.size):
@@ -68,7 +110,7 @@ def grdmse(weights_1, weights_2, temp1, temp2, image_in, image_out):
    return grad_1, grad_2
 
 
-eta = 0.1
+eta = 0.01
 
 weights_1 = 2*np.random.random((30, 257)) - 1
 weights_2 = 2*np.random.random((10, 31)) - 1
@@ -109,13 +151,51 @@ image_output = np.array_split(train_out, 10)
 #       grad1, grad2 = grdmse(weights_1, weights_2, temp1, temp2, image_input[i], image_output[i])
 #       weights_1 = weights_1 - eta*grad1
 #       weights_2 = weights_2 - eta*grad2
-for j in range(10):
-   grad1, grad2 = grdmse(weights_1, weights_2, temp1, temp2, image_matrix, train_out)
-   weights_1 = weights_1 - eta*grad1
-   weights_2 = weights_2 - eta*grad2
+# for j in range(10):
+#    grad1, grad2 = grdmse(weights_1, weights_2, temp1, temp2, image_matrix, train_out)
+#    weights_1 = weights_1 - eta*grad1
+#    weights_2 = weights_2 - eta*grad2
 
 
-   print('MSE_old = ', mse(weights_1, weights_2, image_matrix, train_out))
-   print('Cross entropy = ', mse_logcrossentropy(weights_1, weights_2, image_matrix, train_out))
+#    print('MSE_old = ', mse(weights_1, weights_2, image_matrix, train_out))
+#    print('Cross entropy = ', mse_logcrossentropy(weights_1, weights_2, image_matrix, train_out))
+#    accuracy = 100*np.sum(train_out == MNIST_net(image_matrix, weights_1, weights_2)[0])/len(train_out)
+#    print('Accuracy = ', accuracy)
+
+
+test_matrix = np.zeros((256, len(test_in)))
+for i in np.arange(len(test_in)):
+   test_matrix[:,i] = test_in[i]
+# Add a row on ones to the image matrix
+test_matrix = np.vstack((test_matrix, np.ones(len(test_in))))
+
+
+b_max, b, a = MNIST_net(image_matrix, weights_1, weights_2)
+print(np.max(b, axis=0), np.sort(b, axis=0)[-2, :])
+
+eta = 1e-1
+for j in range(50):
+   tel = 0
+   for i in range(len(train_out)):
+      grad1, grad2 = mse_backprop(weights_1, weights_2, image_matrix[:, i], train_out[i])
+      weights_1 -=  eta*grad1
+      weights_2 -=  eta*grad2
+      if ((np.sum(grad1) == 0) & (np.sum(grad2) == 0)):
+         tel += 1
+
+
+
+   # print('MSE_old = ', mse(weights_1, weights_2, image_matrix, train_out))
+   # print('Cross entropy = ', mse_logcrossentropy(weights_1, weights_2, image_matrix, train_out))
    accuracy = 100*np.sum(train_out == MNIST_net(image_matrix, weights_1, weights_2)[0])/len(train_out)
    print('Accuracy = ', accuracy)
+   # print("tel accuracy = ", tel / len(train_out))
+
+   # b_max, b, a = MNIST_net(image_matrix, weights_1, weights_2)
+   # print(np.max(b, axis=0), np.sort(b, axis=0)[-2, :], np.array([b[m, p] for p, m in enumerate(train_out)]))
+
+
+
+
+   accuracy = 100*np.sum(test_out == MNIST_net(test_matrix, weights_1, weights_2)[0])/len(test_out)
+   print('Test Accuracy = ', accuracy)
