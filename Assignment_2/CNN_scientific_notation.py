57dab49ec0fe030e436bc47219345bdb3e54202b
Joey Braspenning <joeybraspenning@gmail.com>
2019-04-15 11:57:14 +0200

    test

--- Assignment_2/CNN_scientific_notation.py
+++ Assignment_2/CNN_scientific_notation.py
@@ -261,6 +261,7 @@ for iteration in range(1, 200):
         rowx = np.squeeze(rowx, axis=3)
         rowy = np.squeeze(rowy, axis=3)
         preds = np.squeeze(preds, axis=3)
+        print(type(rowx), type(np.array(rowx)))
         q = ctable.decode(rowx[0])
         correct = ctable.decode(rowy[0])
         guess = ctable.decode(preds[0], calc_argmax=False)
