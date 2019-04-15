f0e9673b51fc7f579a1edf41eb202c0782d89040
Joey Braspenning <joeybraspenning@gmail.com>
2019-04-15 15:26:45 +0200

    Added more decimals

--- Assignment_2/CNN_logarithmic.py
+++ Assignment_2/CNN_logarithmic.py
@@ -107,7 +107,7 @@ def generate_data_set(n_items, input_len, n_decimals):
 TRAINING_SIZE = 5000
 TEST_SIZE = 1000
 INPUT_LEN = 10 # The maximum number of digits in the input integers
-DECIMALS = 3 # the number of decimals in the scientific notation
+DECIMALS = 6 # the number of decimals in the scientific notation
 
 # This number is fixed
 OUTPUT_LEN = 4 + DECIMALS
