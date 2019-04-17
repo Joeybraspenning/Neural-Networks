036d278e25edb3ed5f5523417ad8a5ad759f02b3
Joey Braspenning <joeybraspenning@gmail.com>
2019-04-17 22:30:12 +0200

    Changed architecture

--- Assignment_2/CNN_hexadecimal.py
+++ Assignment_2/CNN_hexadecimal.py
@@ -210,7 +210,7 @@ for activ in ['relu']:
     # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE.
     # Note: In a situation where your input sequences have a variable length,
     # use input_shape=(None, num_feature).
-    model.add(Conv2D(256, input_shape = (INPUT_LEN, len(chars), 1), kernel_size=(5, 9), padding='same'))
+    model.add(Conv2D(512, input_shape = (INPUT_LEN, len(chars), 1), kernel_size=(5, 9), padding='same'))
     model.add(BatchNormalization(center=True, scale=True))
     model.add(Activation(activ))
     model.add(Dropout(0.25))
