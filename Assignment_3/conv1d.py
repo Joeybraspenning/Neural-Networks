885f54ed43e40d589ad6f269284484067126c2de
J.R. Braspenning <s1508768@leidenuniv.nl>
2019-05-01 09:24:38 +0200

    a3

---
+++ Assignment_3/conv1d.py
@@ -0,0 +1,5 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.datasets import mnist
+from keras.layers.normalization import BatchNormalization
