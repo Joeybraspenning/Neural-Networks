055e7f12d2f088c9abddc1a5302828c24f0b4b2a
Joey Braspenning <braspenning@student29.strw.leidenuniv.nl>
2019-05-17 16:06:52 +0200

    add

--- Assignment_3/plot.py
+++ Assignment_3/plot.py
@@ -12,12 +12,20 @@ def load_obj(name ):
 
 
 def plot_accuracies():
-	for i in range(1):
-		hist = load_obj('history_{}'.format(i))
-		plt.plot(range(len(hist['acc'])), hist['acc'])
-		plt.plot(range(len(hist['val_acc'])), hist['val_acc'], linestyle='--')
-		plt.xlabel('Epochs')
-		plt.ylabel('Accuracy')
-	plt.show()
+	cols = ['y', 'b', 'c', 'g', 'k', 'r', 'm']
+	name = [r'CO', r'$\mathrm{CO_2}$', r'$\mathrm{CH_4}$', r'$\mathrm{Na}$', r'$\mathrm{K}$', r'$\mathrm{NH_3}$', r'$\mathrm{PH_3}$']
+	for i in range(7):
+		hist = load_obj('history_exp1_categorize_{}'.format(i))
+		print(name[i], np.mean(hist['val_acc'][50:]))
+	# 	plt.plot(range(len(hist['acc'])), hist['acc'], color=cols[i], label=name[i])
+	# 	plt.plot(range(len(hist['val_acc'])), hist['val_acc'], linestyle='--', color=cols[i], alpha=0.5, linewidth=1)
+	# plt.legend()
+	# plt.xlabel('Epochs', fontsize=20)
+	# plt.ylabel('Accuracy', fontsize=20)
+	# plt.xticks(fontsize=15)
+	# plt.yticks(fontsize=15)
+	# plt.tight_layout()
+	# plt.savefig('exp3_categorize.pdf')
+	# plt.show()
 
 plot_accuracies()
